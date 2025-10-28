9c4fcc4121e0dfbb19fa27bf4accde07ba715074
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-10-28 15:59:16 +0530

    created the project structure of console app by using .net 8.0

---
+++ BridgeLabzApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace BridgeLabzApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
