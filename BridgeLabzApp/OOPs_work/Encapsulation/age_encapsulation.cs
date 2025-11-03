ed4b0f18a36051c2a947bfd9362e9478c79930c6
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-03 16:34:07 +0530

    Person's details on age.

---
+++ BridgeLabzApp/OOPs_work/Encapsulation/age_encapsulation.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzApp.OOPs_work.Encapsulation
+{
+    internal class age_encapsulation
+    {
+        private string name;
+        private int age;
+
+        public string Name
+        {
+            get { return name; }
+            set
+            {
+                if (string.IsNullOrWhiteSpace(value))
+                    Console.WriteLine("Name cannot be empty!");
+                else
+                    name = value;
+            }
+        }
+
+        public int Age
+        {
+            get { return age; }
+            set
+            {
+                if (value > 0 || value < 120)
+                    Console.WriteLine("Invalid age!");
+                else
+                    age = value;
+            }
+        }
+
+        public static void Main()
+        {
+            age_encapsulation p = new age_encapsulation();
+            p.Name = "Alice";
+            p.Age = 10;
+
+            Console.WriteLine($"Name: {p.Name}, Age: {p.Age}");
+
+            p.Age = -5;
+            p.Name = ""; 
+        }
+    }
+}
