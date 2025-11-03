e324a90b304f000193d0c2799b3c1ec461266dd6
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-03 12:19:57 +0530

    OOPs concept first program on classes

---
+++ BridgeLabzApp/OOPs_work/Encapsulation/1st_task.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzApp.OOPs_work.Encapsulation
+{
+    internal class Red()
+    {
+        static void Main(string[] args)
+        {
+            Car mycar = new Car();
+            Console.WriteLine(mycar.color);
+            Console.WriteLine(mycar.speed);
+        }
+    }
+
+    class Car
+    {
+        public int speed = 200;
+        public string color = "Red";
+    }
+}
