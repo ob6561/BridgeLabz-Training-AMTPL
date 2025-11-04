71e0909bd915be16745750e17bfe92daed906850
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-04 15:26:19 +0530

    Encapsulation 2nd program.

---
+++ BridgeLabzApp/OOPs_work/Encapsulation/BankAccount.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace BridgeLabzApp.OOPs_work.Encapsulation
+{
+    internal class BankAccount
+    {
+        private double balance;
+
+        public BankAccount(double initialBalance)
+        {
+            balance = initialBalance;
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount > 0)
+            {
+                balance += amount;
+                Console.WriteLine($"Deposited: {amount}, New Balance: {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Deposit amount must be positive.");
+            }
+        }
+
+        public void Withdraw(double amount)
+        {
+            if (amount > 0 && amount <= balance)
+            {
+                balance -= amount;
+                Console.WriteLine($"Withdrew: {amount}, New Balance: {balance}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid withdrawal amount.");
+            }
+        }
+
+        public double Balance => balance;
+    }
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(1000);
+            account.Deposit(500);
+            account.Withdraw(200);
+            Console.WriteLine($"Final Balance: {account.Balance}");
+        }
+    }
+}
