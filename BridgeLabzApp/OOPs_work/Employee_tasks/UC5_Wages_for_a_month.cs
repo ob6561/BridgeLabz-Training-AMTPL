5010e17894c1984a5f2c2442303baaaa3512046c
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-06 11:44:10 +0530

    UC5

---
+++ BridgeLabzApp/OOPs_work/Employee_tasks/UC5_Wages_for_a_month.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzApp.OOPs_work.Employee_tasks
+{
+    internal class UC5_Wages_for_a_month
+    {
+        public static void Main(string[] args)
+        {
+            public const int IS_PART_TIME = 1;
+            public const int IS_FULL_TIME = 2;
+            int EMP_RATE_PER_HOUR = 20;
+            int empHrs = 0;
+            int numberOfWorkingDays = 20;
+            int empWage = 0;
+            
+
+    }
+    }
+}
