5420d2af26a0df72b74b1be89ce0931c6e23e877
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-11 13:45:25 +0530

    more problems on abstraction

---
+++ BridgeLabzApp/OOPs_work/Abstraction/Abstract_and_interface.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzApp.OOPs_work.Abstraction
+{
+    abstract class Appliance
+    {
+        public abstract void TurnOn();
+    }
+
+    interface IRemoteControl
+    {
+        void TurnOff();
+    }
+    class SmartTV : Appliance, IRemoteControl
+    {
+        public override void TurnOn()
+        {
+            Console.WriteLine("Smart TV turned ON using power button.");
+        }
+
+        public void TurnOff()
+        {
+            Console.WriteLine("Smart TV turned OFF using remote control.");
+        }
+    }
+    internal class Abstract_and_interface
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Task 5: Abstract Class + Interface Together");
+
+            Appliance tvAppliance = new SmartTV();
+            tvAppliance.TurnOn();
+
+            // Casting to interface to call TurnOff()
+            IRemoteControl tvRemote = (IRemoteControl)tvAppliance;
+            tvRemote.TurnOff();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ BridgeLabzApp/OOPs_work/Abstraction/Shapes.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzApp.OOPs_work.Abstraction
+{
+    abstract class Shape
+    {
+        public abstract double CalculateArea();
+    }
+
+    class Circle : Shape
+    {
+        private double radius;
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+
+        public override double CalculateArea()
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+
+    class Rectangle : Shape
+    {
+        private double width;
+        private double height;
+
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+
+        public override double CalculateArea()
+        {
+            return width * height;
+        }
+    }
+    internal class Shapes
+    {
+        static void Main(string[] args)
+        {
+            Shape circle = new Circle(5);
+            Shape rectangle = new Rectangle(4, 6);
+
+            
+            Console.WriteLine($"Circle Area: {circle.CalculateArea()}");
+            Console.WriteLine($"Rectangle Area: {rectangle.CalculateArea()}");
+
+            Console.ReadLine();
+        }
+    }
+}
