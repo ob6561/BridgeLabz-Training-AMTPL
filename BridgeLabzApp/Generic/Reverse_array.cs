dddeb8eaf7e885120613bf0d175683c615652583
Om_Bandyopadhyay <ombandyopadhyay@gmail.com>
2025-11-18 12:13:42 +0530

    Array reversed

---
+++ BridgeLabzApp/Generic/Reverse_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BridgeLabzApp.Generic
+{
+    internal class Reverse_array
+    {
+        public static T[] ReverseArrayCopy<T>(T[] array)
+        {
+            T[] reversed = new T[array.Length];
+            int j = 0;
+
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                reversed[j] = array[i];
+                j++;
+            }
+
+            return reversed;
+        }
+
+    }
+}
--- BridgeLabzApp/Program.cs
+++ BridgeLabzApp/Program.cs
@@ -1,10 +1,28 @@
-﻿namespace BridgeLabzApp
+﻿using System;
+using BridgeLabzApp.Generic;   // <-- Add this so you can access Reverse_array class
+
+namespace BridgeLabzApp
 {
-    internal class Program
+    class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int[] arr = { 1, 2, 3, 4, 5 };
+
+            Console.WriteLine("Original array:");
+            foreach (var item in arr)
+                Console.Write(item + " ");
+
+            Console.WriteLine();
+
+            // Call your reverse method
+            int[] reversed = Reverse_array.ReverseArrayCopy(arr);
+
+            Console.WriteLine("Reversed array:");
+            foreach (var item in reversed)
+                Console.Write(item + " ");
+
+            Console.ReadLine();
         }
     }
 }
